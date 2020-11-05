37dd9e754ebb75a904be25dcc27de60635213f44
Wayne 王維靖 <weijing329@outlook.com>
2020-11-05 13:39:30 +0800

    test DynamicData subscribe on remoteTask update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DynamicData;
 
 namespace DotNetCoreGoogleCloudPubSubSimpleClient
 {
-    class Program
+  public class RemoteTask
+  {
+    public Guid id;
+    public string name;
+    public bool completed;
+  }
+  class Program
+  {
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      MainAsync().Wait();
     }
+    static async Task MainAsync()
+    {
+      var remoteTasksCache = new SourceCache<RemoteTask, Guid>(remoteTask => remoteTask.id);
+
+      // add task to cache
+      RemoteTask remoteTask1 = new RemoteTask() { id = Guid.NewGuid(), name = "task1", completed = false };
+      remoteTasksCache.AddOrUpdate(remoteTask1);
+
+      // subscribe to task id events
+      remoteTasksCache.Connect()
+                      .Filter(remoteTask => remoteTask.id == remoteTask1.id)
+                      .OnItemUpdated((current, previous) =>
+                      {
+                        Console.WriteLine($"remoteTask: {current.id}, completed = {current.completed}");
+                      })
+                      .Subscribe();
+
+      // update task by id
+      remoteTask1.completed = true;
+      remoteTasksCache.AddOrUpdate(remoteTask1);
+    }
+  }
 }
